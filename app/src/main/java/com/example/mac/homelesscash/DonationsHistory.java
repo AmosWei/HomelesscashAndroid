6219917d9a5d872fcf3738a7f92aaff8000fee2c
Amos <amoswei525@gmail.com>
2019-10-08 12:41:36 +1100

    change login in UI

--- app/src/main/java/com/example/mac/homelesscash/DonationsHistory.java
+++ app/src/main/java/com/example/mac/homelesscash/DonationsHistory.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DonationsHistory extends AppCompatActivity {
+//TODO
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
