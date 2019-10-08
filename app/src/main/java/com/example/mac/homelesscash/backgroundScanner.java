3a72ccca2b8c2551beff1c000a7560c1d8e6c224
jaryd <u5799628@anu.edu.au>
2019-10-08 12:16:35 +1100

    Added background scanner -Currently needs to be reset each time -Does not persist on phone restart(in progress) -launched independently from main application ie, begins scanning before user has signed in (needs check for auto sign in etc.)

---
+++ app/src/main/java/com/example/mac/homelesscash/backgroundScanner.java
@@ -0,0 +1,189 @@
+package com.example.mac.homelesscash;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.NotificationCompat;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Random;
+
+public class backgroundScanner extends Service implements BeaconConsumer {
+
+    private static final String TAG = "BEACON_PROJECT";
+
+    private RegionBootstrap regionBootstrap;
+    private BeaconManager beaconManager;
+
+    private ArrayList<String> beaconList;
+    private ListView beaconListView;
+    private ArrayAdapter<String> adapter;
+    private Random rand;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        this.beaconManager = BeaconManager.getInstanceForApplication(this);
+        this.beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+
+        this.beaconManager.bind(this);
+
+        Notification.Builder newBuilder = new Notification.Builder(this);
+        newBuilder.setSmallIcon(R.drawable.black_scanner1);
+        newBuilder.setContentTitle("Scanning for Beacons");
+        newBuilder.setContentText("Homeless cash project is on the lookout");
+        Intent always_on_intent = new Intent(this, HomePage.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, always_on_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        newBuilder.setContentIntent(pendingIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Log.d("notification_channel", "was created");
+            NotificationChannel channel = new NotificationChannel("BackgroundScanner", "Setup for foreground service", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Foreground service launched when accessing login class");
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+            newBuilder.setChannelId(channel.getId());
+            //notificationManager.notify(456, newBuilder.build());
+        }
+
+
+        startForeground(1, newBuilder.build());
+
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        RangeNotifier rangeNotifier = new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.d("Beacon_Range", "didRangeBeaconsInRegion called with beacon count:  " + beacons.size());
+                    Beacon beacon = beacons.iterator().next();
+                    notifyUser(beacons.toString());
+                }
+            }
+        };
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("MyRangeID", null, null, null));
+            beaconManager.addRangeNotifier(rangeNotifier);
+            beaconManager.startRangingBeaconsInRegion(new Region("MyRangeID", null, null, null));
+            beaconManager.addRangeNotifier(rangeNotifier);
+
+
+        } catch (RemoteException e) {
+            Log.d(TAG, e.toString());
+        }
+
+
+    }
+
+    private void notifyUser(String s) {
+        int exists = 0;
+        int notificationID = 1000;
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            try {
+                StatusBarNotification[] notifications = notificationManager.getActiveNotifications();
+                for (StatusBarNotification notification : notifications) {
+                    if (notification.getId() == notificationID) {
+                        exists = 1;
+                    }
+                }
+                    if (exists == 0){
+                        Log.d("ENTERED", "no current notification");
+
+                        Notification.Builder newBuilder = new Notification.Builder(this);
+                        newBuilder.setSmallIcon(R.drawable.black_scanner2);
+                        newBuilder.setContentTitle("Signal Found");
+                        newBuilder.setContentText("There are possibly homeless nearby! Click here to donate now!");
+                        Intent always_on_intent = new Intent(this, ibeacon_scan.class);
+                        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, always_on_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                        newBuilder.setContentIntent(pendingIntent);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            Log.d("notification_channel", "was created");
+                            NotificationChannel channel = new NotificationChannel("BackgroundScannerNotify", "notifyUser", NotificationManager.IMPORTANCE_DEFAULT);
+                            channel.setDescription("Posted when beacon detected");
+                            notificationManager.createNotificationChannel(channel);
+                            newBuilder.setChannelId(channel.getId());
+                            notificationManager.notify(notificationID, newBuilder.build());
+
+                        }
+
+
+                }
+
+            } catch (Throwable e) {
+                Log.w(TAG, e);
+            }
+
+        }
+
+    }
+//        Notification.Builder newBuilder = new Notification.Builder(this);
+//        newBuilder.setSmallIcon(R.drawable.black_scanner1);
+//        newBuilder.setContentTitle("Scanning for Beacons");
+//        newBuilder.setContentText("Homeless cash project is on the lookout");
+//        Intent always_on_intent = new Intent(this, HomePage.class);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, always_on_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//        newBuilder.setContentIntent(pendingIntent);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+//            Log.d("notification_channel","was created");
+//            NotificationChannel channel = new NotificationChannel("BackgroundScanner", "Setup for foreground service", NotificationManager.IMPORTANCE_DEFAULT);
+//            channel.setDescription("Foreground service launched when accessing login class");
+//            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//            notificationManager.createNotificationChannel(channel);
+//            newBuilder.setChannelId(channel.getId());
+//           // notificationManager.notify(456, newBuilder.build());
+//        }
+//        startForeground(1, newBuilder.build());
+//        //beaconManager.setEnableScheduledScanJobs(false);
+//        //beaconManager.setBackgroundBetweenScanPeriod(0);
+//        //beaconManager.setBackgroundScanPeriod(1100);
+//       // beaconManager.enableForegroundServiceScanning(newBuilder.build(), 456);
+//
+//
+//        Log.d(TAG, "backgroundtask is running");
+//        //beaconManager.bind(this);
+}
