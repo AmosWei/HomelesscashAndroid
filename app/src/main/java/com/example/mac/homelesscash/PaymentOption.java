480bfbfc7064fb4e8d7aa89be4bba0f96e8cd647
hubertdou <694664453@qq.com>
2019-10-09 08:25:05 +1100

    payment option new

--- app/src/main/java/com/example/mac/homelesscash/PaymentOption.java
+++ app/src/main/java/com/example/mac/homelesscash/PaymentOption.java
@@ -6,14 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 public class PaymentOption extends AppCompatActivity {
     Button cnButton;
+    RadioButton visaRadio, paypalRadio, wwRadio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_option);
+        visaRadio = (RadioButton)findViewById(R.id.visaCheckBox);
+        paypalRadio = (RadioButton)findViewById(R.id.paypalCheckBox);
+        wwRadio = (RadioButton)findViewById(R.id.wwCheckBox);
         cnButton = (Button)findViewById(R.id.cnButton);
         cnButton.setOnClickListener(new View.OnClickListener() {
             @Override
