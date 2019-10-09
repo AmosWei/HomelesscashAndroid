ed97eaf36af4b4177a13abef3cedc9e9e3177a7c
jaryd <u5799628@anu.edu.au>
2019-10-09 15:25:13 +1100

    - Fixed Login Service Launcher - Now launches from HomePage.class

--- app/src/main/java/com/example/mac/homelesscash/HomePage.java
+++ app/src/main/java/com/example/mac/homelesscash/HomePage.java
@@ -1,21 +1,70 @@
 package com.example.mac.homelesscash;
 
+import android.app.PendingIntent;
 import android.content.Intent;
 //import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.TextView;
 
-public class HomePage extends AppCompatActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+
+import static android.app.PendingIntent.getActivity;
+
+public class HomePage extends AppCompatActivity implements BootstrapNotifier {
     TextView donations;
     TextView donationHistory;
     TextView donationmap;
     TextView beaconscanner;
     TextView usernameTV;
     TextView people;
+
+    //BeaconManager
+    private BeaconManager beaconManager;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Service initializer code//
+        this.beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        Intent intent = new Intent(this, backgroundScanner.class);
+        //PendingIntent pendingIntent = new PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        ContextCompat.startForegroundService(this, intent);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         super.onCreate(savedInstanceState);
 //        FacebookSdk.sdkInitialize(getApplicationContext());
 //        AppEventsLogger.activateApp(this);
@@ -65,4 +114,18 @@ public class HomePage extends AppCompatActivity {
 
     }
 
+    @Override
+    public void didEnterRegion(Region region) {
+        //null
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+        //null
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int i, Region region) {
+        //null
+    }
 }
